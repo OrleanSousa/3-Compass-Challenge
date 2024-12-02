1a5e8eb258648f48d3b0d0c4289505f9cdc0b8f3
Orlean Sousa <99688852+OrleanSousa@users.noreply.github.com>
2024-12-02 15:11:26 -0300

    Feat: Project init

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import Header from '../components/header/Header'
+import Footer from '../components/footer/Footer'
+
+const Home = () => {
+  return (
+    <>
+    <Header/>
+    <Footer/>
+    </>
+  )
+}
+
+export default Home
