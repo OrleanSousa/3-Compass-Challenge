4f0f136ba01f35679b9134a8674825e669445459
Orlean Sousa <99688852+OrleanSousa@users.noreply.github.com>
2024-12-03 11:44:41 -0300

    feat: creat folder sections homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import Header from '../components/header/Header'
 import Footer from '../components/footer/Footer'
+import MainSection from '../components/homepage/mainsection/MainSection'
 
 const Home = () => {
   return (
     <>
     <Header/>
+    <MainSection/>
     <Footer/>
     </>
   )
