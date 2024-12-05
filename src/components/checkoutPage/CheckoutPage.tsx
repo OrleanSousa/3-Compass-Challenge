80181e255d58a511e698c87e5d274f4373a9d61a
Orlean Sousa <99688852+OrleanSousa@users.noreply.github.com>
2024-12-05 09:59:27 -0300

    refact: refactor files contact and singleProduct

--- src/components/checkoutPage/CheckoutPage.tsx
+++ src/components/checkoutPage/CheckoutPage.tsx
@@ -1,17 +1,9 @@
-import React from 'react'
-import Header from '../header/Header'
-import SubHeader from '../subHeader/SubHeader'
-import Footer from '../footer/Footer'
-import logoSite from '../../assets/logosite.svg';
-import Qualitys from '../qualitys/Qualitys';
+
 
 const CheckoutPage = () => {
   return (
     <>
-      <Header/>
-      <SubHeader title='Cart' breadcrumb={["Home", "Checkout"]} logo={logoSite}/>
-      <Qualitys/>
-      <Footer/>
+      qualquer coisa
     </>
   )
 }
--- src/pages/CheckOut.tsx
+++ src/pages/CheckOut.tsx
@@ -1,9 +1,18 @@
 import CheckoutPage from '../components/checkoutPage/CheckoutPage'
+import Header from '../components/header/Header'
+import SubHeader from '../components/subHeader/SubHeader'
+import logoSite from '../assets/logosite.svg';
+import Qualitys from '../components/qualitys/Qualitys';
+import Footer from '../components/footer/Footer';
 
 const CheckOut = () => {
   return (
     <>
+        <Header/>
+        <SubHeader title='Checkout' breadcrumb={["Home", "Checkout"]} logo={logoSite}/>
         <CheckoutPage />
+        <Qualitys/>
+        <Footer/>
     </>
   )
 }
