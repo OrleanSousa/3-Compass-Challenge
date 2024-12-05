795c7cb5b6e71180d0477423cd7b2529f54f2cea
Orlean Sousa <99688852+OrleanSousa@users.noreply.github.com>
2024-12-05 09:53:08 -0300

    fix: refactor contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Contacts from '../components/contacts/Contacts'
+import Header from '../components/header/Header'
+import SubHeader from '../components/subHeader/SubHeader'
+import Qualitys from '../components/qualitys/Qualitys'
+import Footer from '../components/footer/Footer'
+import logoSite from '../assets/logosite.svg'
 
 const Contact = () => {
   return (
     <>
-    <Contacts/>
+    <Header/>
+      <SubHeader title='Contact' breadcrumb={["Home", "Contact "]} logo={logoSite}/>
+      <Contacts/>
+      <Qualitys/>
+      <Footer/>
     </>
   )
 }
