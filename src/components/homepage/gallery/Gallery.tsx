fd3613f849ab743e859f3967a5ed78f62d12f26c
Orlean Sousa <99688852+OrleanSousa@users.noreply.github.com>
2025-02-19 07:58:21 -0300

    Update Gallery.tsx
    changing image links from aws s3 to drive (lost access to s3)

--- src/components/homepage/gallery/Gallery.tsx
+++ src/components/homepage/gallery/Gallery.tsx
@@ -7,15 +7,15 @@ const Gallery = () => {
             <h2 className='text-center text-[40px] font-bold'>#FuniroFurniture</h2>
         </div>        
         <div className="flex relative mt-[-21px] ">
-            <div className="absolute mt-0 h-[382px]"><img src="https://orlean2024.s3.us-east-2.amazonaws.com/homepageimg/gallery/Rectangle+36.png" alt="" /></div>
-            <div className="relative ml-0 mt-[398px] h-[323px]"><img src="https://orlean2024.s3.us-east-2.amazonaws.com/homepageimg/gallery/Rectangle+37.png" alt="" /></div>
-            <div className="absolute w-[451px] h-[312px] mt-[70px] ml-[116px]"><img src="https://orlean2024.s3.us-east-2.amazonaws.com/homepageimg/gallery/Rectangle+38.png" alt="" /></div>
-            <div className="absolute w-[344px] h-[242px] mt-[398px] ml-[233px]"><img src="https://orlean2024.s3.us-east-2.amazonaws.com/homepageimg/gallery/Rectangle+39.png" alt="" /></div>
-            <div className="absolute w-[295px] h-[392px] mt-[156px] ml-[593px]"><img src="https://orlean2024.s3.us-east-2.amazonaws.com/homepageimg/gallery/Rectangle+40.png" alt="" /></div>
-            <div className="absolute w-[178px] h-[242px] mt-[463px] ml-[904px]"><img src="https://orlean2024.s3.us-east-2.amazonaws.com/homepageimg/gallery/Rectangle+41.png" alt="" /></div>
-            <div className="absolute w-[290px] h-[348px] mt-[72px] ml-[904px]"><img src="https://orlean2024.s3.us-east-2.amazonaws.com/homepageimg/gallery/Rectangle+43.png" alt="" /></div>
-            <div className="absolute w-[258px] h-[196px] mt-[463px] ml-[1098px]"><img src="https://orlean2024.s3.us-east-2.amazonaws.com/homepageimg/gallery/Rectangle+44.png" alt="" /></div>
-            <div className="absolute top-[16px] left-[1226px] w-[425px] h-[433px]"><img src="https://orlean2024.s3.us-east-2.amazonaws.com/homepageimg/gallery/Rectangle+45.png" alt=""/></div>
+            <div className="absolute mt-0 h-[382px]"><img src="https://drive.google.com/file/d/18O5Ozb09a21VhvwUf1KvOugVIGJq1clg/view" alt="" /></div>
+            <div className="relative ml-0 mt-[398px] h-[323px]"><img src="https://drive.google.com/file/d/11snyF0McrnJrhFSghc0m0CEXRuZmi8x2/view" alt="" /></div>
+            <div className="absolute w-[451px] h-[312px] mt-[70px] ml-[116px]"><img src="https://drive.google.com/file/d/1lO7ZAR4S_XNQYt77-0lZdBDxEnbTmEym/view" alt="" /></div>
+            <div className="absolute w-[344px] h-[242px] mt-[398px] ml-[233px]"><img src="https://drive.google.com/file/d/1KRL9jwcMcEIbqp4orHxEJng4-e1a9lsC/view" alt="" /></div>
+            <div className="absolute w-[295px] h-[392px] mt-[156px] ml-[593px]"><img src="https://drive.google.com/file/d/1OSjRPtZTX0NE-JyGHzMPuPvuPAfOjHuQ/view" alt="" /></div>
+            <div className="absolute w-[178px] h-[242px] mt-[463px] ml-[904px]"><img src="https://drive.google.com/file/d/13wCynYhtFAtukp0xOo7815HjjpO3D-kn/view" alt="" /></div>
+            <div className="absolute w-[290px] h-[348px] mt-[72px] ml-[904px]"><img src="https://drive.google.com/file/d/1m7O6un89MpNh2A7sYp8yll8U9Y7PWBzV/view" alt="" /></div>
+            <div className="absolute w-[258px] h-[196px] mt-[463px] ml-[1098px]"><img src="https://drive.google.com/file/d/1ILP-EZDYRLXdGDp15QVbwVUGCeqEWvTF/view" alt="" /></div>
+            <div className="absolute top-[16px] left-[1226px] w-[425px] h-[433px]"><img src="https://drive.google.com/file/d/1E1F3iJd0Q0q1MazhHCmiVxQ3g1yT3ywl/view" alt=""/></div>
         </div>
     </div>
   )
